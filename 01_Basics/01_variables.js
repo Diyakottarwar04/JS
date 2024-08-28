811eae0c9aba6bfef18236993f8fc51baf9da518
DIYA KOTTARWAR <153450255+Diyakottarwar04@users.noreply.github.com>
2024-08-28 17:24:36 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =123456
+let accountEmail="xyz@gmail.com"
+var accountPassword="12345"
+accountcity="pune"
+
+
+// accountId =2    not allowed
+accountEmail="deyya@gmail.com"
+accountPassword="367899"
+accountcity="nagpur"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountcity])
