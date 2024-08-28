b9fda39c2dc1177b00766061cb495eb61443f9ac
DIYA KOTTARWAR <153450255+Diyakottarwar04@users.noreply.github.com>
2024-08-28 17:49:09 +0000

    container config done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert("heyy diya");
+
+//code readability  should be high.
+
+
+let name="diya";
+let age=21;
+let isLoggedIn=false;
+
+/**
+ *number =>2
+ bigInt
+ string => ""
+ boolean = true or false
+ null => standalone value
+ undefined 
+ symbol => unique
+ */
